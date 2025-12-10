4811ee27747ea69a37ddbe9bcf63c2e058a44bfa
Hoang Loc <hoangphuocloc.phurieng@gmail.com>
2025-12-10 10:02:03 +0700

    update file contract adress

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -1,7 +1,5 @@
 import ChatABI from '../contracts/BasicChat.json';
 
-export const CONTRACT_ADDRESS =
-  process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ||
-  '0x985105Ba9dF9246F30875B8B978a626CBcF3E14B';
+export const CONTRACT_ADDRESS = '0x985105Ba9dF9246F30875B8B978a626CBcF3E14B';
 
 export const ABI_CHAT_BASIC = ChatABI.abi;
